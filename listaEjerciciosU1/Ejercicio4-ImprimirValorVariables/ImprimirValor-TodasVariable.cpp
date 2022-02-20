e97bfedf0b5c92f2ec71eab1b668ab6a8a2d37fc
Josue Ventura <josueventurajvo@gmail.com>
2022-02-20 08:14:12 -0600

    Todas las Variables

---
+++ listaEjerciciosU1/Ejercicio4-ImprimirValorVariables/ImprimirValor-TodasVariable.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bool valorBoolean;
+    valorBoolean = true;
+    
+    double valorDouble = 10.25;
+    char valorChar = 'A';
+    string valorString = " He vuelto ";
+    int valorInt = 10;
+
+    cout << " Valor Bool: " << valorBoolean << endl;
+    cout << " Valor Char: " << valorChar << endl;
+    cout << " Valor Int: " << valorInt << endl;
+    cout << " Valor String: " << valorString << endl;
+    cout << " Valor Double: " << valorDouble << endl;
+
+
+
+    return 0;
+}
