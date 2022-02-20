c6720b4927e934f47d927357f962167f3b18859c
Josue Ventura <josueventurajvo@gmail.com>
2022-02-20 07:43:51 -0600

    VariableEntero

---
+++ listaEjerciciosU1/Ejercicio4-Variables/Variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+
+int variable = 0;
+
+cout << " Enteros: " << endl;
+
+variable = 10;
+cout << " Valor: " << variable << endl;
+
+variable = 15;
+cout << " segundo valor: " << variable << endl;
+
+    return 0;
+}
