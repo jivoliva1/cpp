2f6d5bd91bd2a9025b22a0c6ac456e6686f86dbf
Josue Ventura <josueventurajvo@gmail.com>
2022-02-20 06:52:42 -0600

    Nombre

---
+++ listaEjerciciosU1/Ejercicio2-HolaConNombre/HolaConNombre.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string nombre;
+
+    cout << " Hola! Ingrese su nombre: ";
+    cin >> nombre;
+
+    cout << endl;
+    cout << endl;
+
+    cout << " Muchas gracias " << nombre << "!" << endl;
+
+    return 0;
+}
