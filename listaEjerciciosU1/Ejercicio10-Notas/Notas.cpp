4b2413f51f2b3db174bf84deb93319a7f5e1f589
Josue Ventura <josueventurajvo@gmail.com>
2022-02-22 06:12:48 -0600

    Notas

--- listaEjerciciosU1/Ejercicio10-Notas/Notas.cpp
+++ listaEjerciciosU1/Ejercicio10-Notas/Notas.cpp
@@ -6,7 +6,43 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+
+   //Declaracion de Variables
+    int notas = 0;
     
+    //Proceso Logico
+    cout << " Ingrese su puntuacion Correspondiente : " << endl;
+    cin >> notas;
+   
+    //Salida
+    if ( notas > 100 || notas < 0 ){
+        cout << " Ingrese una nota entre 0 y 100 ";
+        return 0;
+    }
+
+    if ( notas >= 96 && notas <= 100 ){
+        cout << " Tu nota es: A " << endl;
+    }		        
+	 
+    if ( notas >= 91 && notas <= 95 ){
+        cout << " Tu nota es: B " << endl;
+    }
+
+    if ( notas >= 81 && notas <= 90 ){
+        cout << " Tu nota es: C " << endl;
+    }
+
+    if ( notas >= 71 && notas <= 80 ){
+        cout << " Tu nota es: D- "<< endl;
+    }
+
+    if ( notas >= 61 && notas <= 70 ){
+        cout << " Tu nota es: E " << endl;
+    }
+
+    if ( notas < 60 ){
+        cout << " Tu nota es: F " << endl;
+    }  
 
 
     return 0;
