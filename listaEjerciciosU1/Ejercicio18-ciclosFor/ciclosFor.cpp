d608ee3190b00581e99feb5665152005c15b7f9a
Josue Ventura <josueventurajvo@gmail.com>
2022-02-24 04:39:18 -0600

    Ciclo For

---
+++ listaEjerciciosU1/Ejercicio18-ciclosFor/ciclosFor.cpp
@@ -0,0 +1,21 @@
+/*  Josue Ventura
+   Ciclo For  */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int ciclo = 0;
+
+    for ( ciclo = 1; ciclo <= 100; ciclo ++)
+    {
+     
+     cout << " Vive la vida  " << ciclo << endl;
+
+
+    }
+
+
+    return 0;
+}
